f14cb7fbcdfde748206b8831a684688f1fb32378
Daniel Gudiño <dgudino@mary-iancarina.com>
2020-11-17 16:53:15 -0400

    UPD collapse border left

--- src/views/components/Collapse.vue
+++ src/views/components/Collapse.vue
@@ -3,21 +3,18 @@
     <div class="vx-row" v-for="(classes, index) in classesList" :key="index">
       <div class="w-full">
         <div class="flex justify-between">
-          <div class="w-full bg-white">
+          <div class="w-full bg-white rounded-full">
             <div class="flex justify-between">
               <div class="text-center px-4 py-2 m-2">
                 <div class="px-6 py-2">
-                  <div
-                    class=" "
-                    @click="showContent(index)"
-                  >
+                  <div class=" " @click="showContent(index)">
                     <feather-icon
                       icon="CheckIcon"
-                      svgClasses="h-6 w-8 text-success"
-
+                      svgClasses="h-8 w-8 text-success "
                     />
-                    <span class="text-xl text-title font-semibold"> {{ classes.subject.name }}</span>
-
+                    <span class="text-xl text-title font-semibold text-justify">
+                      {{ classes.subject.name }}
+                    </span>
                   </div>
                 </div>
               </div>
@@ -31,41 +28,43 @@
           </div>
         </div>
       </div>
-      <div class="w-full backgroud-internal mx-10 my-3 mt-3" v-if="display">
-        <div class="grid grid-cols-3 divide-x divide-gray-400">
-          <div class="">
-            <div class="flex content-between">
-              <div class="m-4 p-2 rounded-full bg-white">
-                <ListIcon></ListIcon>
-              </div>
-              <div class="w-2/3 p-2">
-                <p>{{ classes.subject.content[0].type }}</p>
-                <h4 class="text-title font-bold">
-                  {{ classes.subject.content[0].title }}
-                </h4>
+      <div class="w-full backgroud-internal-content mx-10">
+        <div class="w-full backgroud-internal mx-10 my-3 mt-3" v-if="display">
+          <div class="grid grid-cols-3 divide-x divide-gray-400">
+            <div class="">
+              <div class="flex content-between">
+                <div class="m-4 p-2 rounded-full bg-white">
+                  <ListIcon></ListIcon>
+                </div>
+                <div class="w-2/3 p-2">
+                  <p>{{ classes.subject.content[0].type }}</p>
+                  <h4 class="text-title font-bold">
+                    {{ classes.subject.content[0].title }}
+                  </h4>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="grid justify-items-center">
-            <div class="w-2/3 p-6">
-              <p class="text-center">
-                {{ classes.subject.content[0].dateend }}
-              </p>
+            <div class="grid justify-items-center">
+              <div class="w-2/3 p-6">
+                <p class="text-center">
+                  {{ classes.subject.content[0].dateend }}
+                </p>
+              </div>
             </div>
-          </div>
-          <div class="grid justify-items-end">
-            <div class="w-2/6 p-4">
-              <div class="rounded-full h-10 p-2">
-                <vs-chip class="bg-white">
-                  <feather-icon
-                    :icon="classes.subject.content[0].icon"
-                    svgClasses="h-5 w-8 text-red"
-                  >
-                  </feather-icon>
-                  <p class="text-center text-red font-semibold">
-                    {{ classes.subject.content[0].status }}
-                  </p>
-                </vs-chip>
+            <div class="grid justify-items-end">
+              <div class="w-2/6 p-4">
+                <div class="rounded-full h-10 p-2">
+                  <vs-chip class="bg-white">
+                    <feather-icon
+                      :icon="classes.subject.content[0].icon"
+                      svgClasses="h-5 w-8 text-red"
+                    >
+                    </feather-icon>
+                    <p class="text-center text-red font-semibold">
+                      {{ classes.subject.content[0].status }}
+                    </p>
+                  </vs-chip>
+                </div>
               </div>
             </div>
           </div>
@@ -95,7 +94,7 @@ export default {
   },
   methods: {
     showContent(index) {
-      console.log(this.display)
+      console.log(this.display);
       //this[index].display = !this.display;
     },
   },
@@ -111,7 +110,10 @@ export default {
 .text-title {
   color: #22215b;
 }
-.success{
-  color:green;
+.success {
+  color: green;
+}
+.backgroud-internal-content {
+  border-left: 2px solid;
 }
 </style>
