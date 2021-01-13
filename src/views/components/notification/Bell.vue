6321f1c6925c592d753241b2b0c689dc2edbde22
Daniel Gudiño <daniel.jabba1@gmail.com>
2021-01-13 19:05:11 -0400

    add static notification Bell

---
+++ src/views/components/notification/Bell.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="vx-col w-full section__color">
+        <h2 class="font-bold text-title pb-4">{{ title }}</h2>
+
+        <div class="flex p-2 body-card rounded space-y-6">
+            <h6 class="text-title">{{ titleAssignment }}</h6>
+
+            <p class="items-start">{{ typeAttach }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NotificationBell',
+  props: {
+  }
+}
+</script>
+
+<style>
+
+</style>
