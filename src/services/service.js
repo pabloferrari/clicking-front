a4affd795ed5084cfb241210c11aa61048e0e042
pabloferrari <pablo.ferrari@me.com>
2021-04-08 10:05:36 -0300

    UPD // Fixes

--- src/services/service.js
+++ src/services/service.js
@@ -4,7 +4,9 @@ import router from '../router'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
+  timeout: 10000,
   headers: {
+    'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Origin': '*'
   }
 })
